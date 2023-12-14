0cdc3c5a46e279d4b96f0b36cfd2f19f8cf1c36d
Pedro Pereira <pdgper@gmail.com>
2023-12-14 18:34:51 +0000

    Built AuthPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/AuthPage/AuthPage";
 import HomePage from "./pages/HomePage/HomePage";
+
 function App() {
   return (
     <>
@@ -7,6 +9,10 @@ function App() {
         <Route
           path="/"
           element={<HomePage />}
+        />
+        <Route
+          path="/auth"
+          element={<AuthPage />}
         />
       </Routes>
     </>
