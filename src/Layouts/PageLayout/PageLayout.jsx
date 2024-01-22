1a3df51d6c0a637effe3d659bf541a52543dc13a
Pedro Pereira <pdgper@gmail.com>
2024-01-22 03:29:15 +0000

    Added Navbar for logged out users

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,6 +1,7 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useLocation } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { auth } from "../../firebase/firebase";
 
@@ -13,8 +14,13 @@ export default function PageLayout({ children }) {
   // sidebar should be rendered, as the pathname check alone
   // still makes it flicker for a split second.
   const canRenderSidebar = pathname !== "/auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
+
+  const checkingUserIsAuth = !user && loading;
+  if (checkingUserIsAuth) return <PageLayoutSpinner />;
+
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
       {/* ---------------- LEFT SIDEBAR ---------------- */}
       {canRenderSidebar ? (
         <Box w={{ base: "70px", md: "240px" }}>
@@ -22,10 +28,21 @@ export default function PageLayout({ children }) {
         </Box>
       ) : null}
 
+      {/* ---------------- NAVBAR ---------------- */}
+      {canRenderNavbar ? <Navbar /> : null}
+
       {/* ---------------- CONTENT ON RIGHT ---------------- */}
-      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }} mx={"auto"}>
         {children}
       </Box>
     </Flex>
   );
 }
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex flexDir={"column"} justifyContent={"center"} alignItems={"center"} h={"100vh"}>
+      <Spinner size="xl" />
+    </Flex>
+  );
+};
